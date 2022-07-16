08a7c090ef0b27acd3e52ad35061094c64a6af77
Cra7y <cra7yy@gmail.com>
2022-07-16 21:53:12 +0300

    fix url api

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,11 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', login);
 app.post('/signup', postUser);
 
